af6890ea195a90e95ef4521379756527c3429a63
amithdm <amithmirihella@gmail.com>
2022-12-16 11:21:20 +0530

    added image gallery

--- src/pages/detailCase.js
+++ src/pages/detailCase.js
@@ -8,6 +8,7 @@ import {
   BackButton,
   BackArrow
 } from "../styles/Case.styles";
+import Gallery from 'react-photo-masonry';
 
 import Reveal from 'react-reveal/Reveal';
 import { withRouter } from "react-router";
@@ -18,6 +19,89 @@ export const ScrollTop = ({ children, location }) => {
   React.useEffect(() => window.scrollTo(0, 0), [location])
   return children
 }
+
+const photos = [
+  {
+    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    width: 1,
+    height: 1
+  },
+  {
+    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/XiDA78wAZVw/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/x8xJpClTvR0/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/qGQNmBE7mYw/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/NuO6iTBkHxE/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/pF1ug8ysTtY/600x400",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/A-fubu9QJxE/800x533",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/5P91SF0zNsI/740x494",
+    width: 4,
+    height: 3
+  }
+];
 
 class Case extends Component {
   constructor(props) {
@@ -112,6 +196,10 @@ class Case extends Component {
                   <span>Home Decoration</span>
                 </li>
               </ul>
+            </div>
+            <div className="col-md-12">
+              <br/>
+            <Gallery photos={photos} direction={"column"} />
             </div>
            
             <div className='col-md-12'>
